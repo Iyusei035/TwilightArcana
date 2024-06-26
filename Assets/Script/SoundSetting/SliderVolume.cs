d3057e77863c4db13082092bcb2004b0eaf4a7bb
ryuuya1231 <kd1344660@st.kobedenshi.ac.jp>
2024-06-26 14:36:51 +0900

    2024/06/26:細かい所を改修、Videoのvecteezy_magical-portal-in-fantasy-forest_1626265を入れたので無事に入っているかの確認をお願いします。

--- Assets/Script/SoundSetting/SliderVolume.cs
+++ Assets/Script/SoundSetting/SliderVolume.cs
@@ -7,11 +7,13 @@ public class SliderVolume : MonoBehaviour
 {
     public void SetBGMVolume(float volume)
     {
+        if (SoundManager.instance == null) return;
         SoundManager.instance.SetBgmVolume(volume);
     }
 
     private void Start()
     {
+        if (SoundManager.instance == null) return;
         gameObject.GetComponent<Slider>().value = SoundManager.instance.GetBgmVolume();
     }
 }
